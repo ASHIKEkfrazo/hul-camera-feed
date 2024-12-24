2d0148ce0d04e2565652323789ab6b85614e1aa3
MgAsma <asma@ekfrazo.in>
2024-12-24 11:20:26 +0530

    camera card size fixed

--- src/Pages/CameraPage.jsx
+++ src/Pages/CameraPage.jsx
@@ -138,7 +138,7 @@ const CameraPage = () => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
         {cameras.map((camera) => (
           <div
             key={camera.id}
